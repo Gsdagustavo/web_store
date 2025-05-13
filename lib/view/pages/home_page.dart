98d32682ab5d092f71ad89b8441c9e56dc3c11d3
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-13 19:56:53 -0300

    made a listview to see all the products fetched from the API

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:web_store/controller/providers/products_provider.dart';
 import 'package:web_store/view/widgets/app_bar.dart';
 
 /// Home page of the app. All the products will be shown in this screen
@@ -8,8 +10,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Hello World!')),
       appBar: MyAppBar(title: 'Home page'),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Consumer<ProductsProvider>(
+          builder: (context, productsProvider, child) {
+            final products = productsProvider.products;
+
+            return RefreshIndicator(
+              onRefresh: () async {
+                await productsProvider.loadProducts();
+              },
+
+              child: ListView.separated(
+                separatorBuilder:
+                    (context, index) => const SizedBox(height: 15),
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  final product = products[index];
+
+                  return ListTile(
+                    title: Text(product.title),
+
+                    subtitle: Text(
+                      product.description,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                    ),
+
+                    leading: Image.network(product.images[0]),
+                    contentPadding: EdgeInsets.all(10),
+                  );
+                },
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
