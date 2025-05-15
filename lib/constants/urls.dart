c181e055e33deaddcd82a7e75e775b4eb107c136
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-14 22:10:09 -0300

    added the login url constant

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,3 +1,6 @@
 /// DummyJson API that contains lots of free JSON's to use as a fake API
 /// This one in specific is a Products API
 const String productsUrl = 'https://dummyjson.com/products';
+
+/// DummyJson endpoint to make login
+const String loginUrl = 'https://dummyjson.com/auth/login';
