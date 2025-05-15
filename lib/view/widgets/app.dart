4f6841140493d69c67fc0509f072bf3cfec2c01e
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-15 19:01:36 -0300

    added a feature that if the user was already logged in before, the login screen is skipped and the homepage is shown instead

--- lib/view/widgets/app.dart
+++ lib/view/widgets/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:web_store/controller/providers/login_provider.dart';
 import 'package:web_store/view/pages/login_page.dart';
 import 'package:web_store/view/pages/product_details_page.dart';
 
@@ -14,8 +15,8 @@ class StoreApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeProvider>(
-      builder: (context, themeProvider, child) {
+    return Consumer2<ThemeProvider, LoginProvider>(
+      builder: (context, themeProvider, loginProvider, child) {
         return MaterialApp(
           title: 'Web Store',
           debugShowCheckedModeBanner: false,
@@ -33,6 +34,7 @@ class StoreApp extends StatelessWidget {
 
           /// Routes
           routes: {
+            '/': (context) => SplashScreen(),
             '/homePage': (context) => const HomePage(),
             '/loginPage': (context) => const LoginPage(),
             '/productDetails': (context) {
@@ -42,10 +44,60 @@ class StoreApp extends StatelessWidget {
             },
           },
 
-          /// Defines the login page as the initial route
-          initialRoute: '/loginPage',
+          /// Start with splash screen that will handle auth
+          initialRoute: '/',
         );
       },
+    );
+  }
+}
+
+/// A splash screen that checks authentication status before
+/// navigating to the appropriate screen
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkAuthentication();
+  }
+
+  Future<void> _checkAuthentication() async {
+    final loginProvider = Provider.of<LoginProvider>(context, listen: false);
+    await loginProvider.loadUser();
+
+    /// If the user is logged in, pushes the Home Page
+    ///
+    /// Otherwise, pushes the Login Page
+    if (loginProvider.isLoggedIn) {
+      Navigator.of(context).pushReplacementNamed('/homePage');
+    } else {
+      Navigator.of(context).pushReplacementNamed('/loginPage');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Web Store',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 24),
+            const CircularProgressIndicator(),
+          ],
+        ),
+      ),
     );
   }
 }
