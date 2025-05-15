753a982acc40edd6e6374035793bc4932ac09063
Gsdasgustavo <gugadanielalvez@gmail.com>
2025-05-15 10:05:01 -0300

    created a page that shows a product's infos

---
+++ lib/view/pages/product_details_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:web_store/view/widgets/app_bar.dart';
+import 'package:web_store/view/widgets/product_image.dart';
+
+import '../../model/product_model.dart';
+
+class ProductDetailsPage extends StatelessWidget {
+  const ProductDetailsPage({super.key, required this.product});
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyAppBar(title: product.title),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ProductImage(
+                imageSrc: product.images.first,
+              ),
+            ),
+
+            const SizedBox(width: 20),
+
+            Expanded(
+              child: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      product.title,
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+
+                    const SizedBox(height: 10),
+
+                    Text(
+                      product.description,
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+
+            IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart))
+          ],
+        ),
+      ),
+    );
+  }
+}
