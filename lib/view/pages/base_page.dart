6c8265abb51e609583d464a0da69ecb29404c58a
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-16 10:01:31 -0300

    documented the Base Page class

--- lib/view/pages/base_page.dart
+++ lib/view/pages/base_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:web_store/view/widgets/app_bar.dart';
 import 'package:web_store/view/widgets/bottom_navigator.dart';
 
+/// This is a Base Page that can be reused in most of the application's pages
 class BasePage extends StatelessWidget {
   const BasePage({
     super.key,
@@ -10,8 +11,13 @@ class BasePage extends StatelessWidget {
     required this.useNavigator,
   });
 
+  /// The body of the scaffold
   final Widget body;
+
+  /// The title that will be shown in the AppBar
   final String title;
+
+  /// Whether the page will have the [MyBottomNavigatorBar] or not
   final bool useNavigator;
 
   @override
