7c8fc8aa2adcd087e448af47cff609242ccdbaa5
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-17 20:10:09 -0300

    created a ListTile (CartItemCard) to show about a product's info in the Cart page

---
+++ lib/view/widgets/cart_item_card.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:web_store/model/cart_item.dart';
+import 'package:web_store/view/widgets/base_image.dart';
+
+class CartItemCard extends StatelessWidget {
+  const CartItemCard({super.key, required this.cartItem});
+
+  final CartItem cartItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+
+      /// Padding
+      contentPadding: EdgeInsets.all(20),
+
+      /// Item's image
+      leading: BaseImage(imageUrl: cartItem.thumbnail),
+
+      /// Item's title
+      title: Text(cartItem.title),
+
+      /// Icon that simulates a "remove" button
+      trailing: Icon(Icons.clear),
+
+      /// Background color
+      tileColor: Theme.of(context).cardColor,
+
+      /// Item's price, quantity and total price
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Price: ${cartItem.price}'),
+          Text('Quantity: ${cartItem.quantity}'),
+          Text('Total: ${cartItem.total}'),
+        ],
+      ),
+    );
+  }
+}
