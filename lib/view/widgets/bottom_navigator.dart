a32fd43aa97e873fa97ff1e773a7978e48273407
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-17 18:30:18 -0300

    added the cart item to the custom bottom navigator bar

--- lib/view/widgets/bottom_navigator.dart
+++ lib/view/widgets/bottom_navigator.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
-/// A custom [BottomNavigatorBar] that contains the [HomePage], [ProfilePage]
-/// and [SettingsPage] items
+/// A custom [BottomNavigatorBar] that contains the [HomePage], [ProfilePage],
+/// [CartPage] and [SettingsPage] items
 class MyBottomNavigatorBar extends StatelessWidget {
   const MyBottomNavigatorBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      /// Defines the three pages icons and labels
+      type: BottomNavigationBarType.fixed,
+      /// Defines the pages icons and labels
       items: [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
         BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
       ],
@@ -22,12 +24,13 @@ class MyBottomNavigatorBar extends StatelessWidget {
           case 0:
             Navigator.pushReplacementNamed(context, '/homePage');
             break;
-
-          /// TODO: Create the Profile Page and the Settings Page
           case 1:
-            Navigator.pushReplacementNamed(context, '/profilePage');
+            Navigator.pushReplacementNamed(context, '/cartPage');
             break;
           case 2:
+            Navigator.pushReplacementNamed(context, '/profilePage');
+            break;
+          case 3:
             Navigator.pushReplacementNamed(context, '/settingsPage');
             break;
         }
