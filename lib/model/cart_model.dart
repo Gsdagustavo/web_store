1eb3f36f3fe5574dbcda0127739798a4df25bb6e
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-20 18:35:08 -0300

    documented the Cart class

--- lib/model/cart_model.dart
+++ lib/model/cart_model.dart
@@ -2,6 +2,18 @@ import 'package:collection/collection.dart';
 
 import 'cart_item.dart';
 
+/// This class represents a [Shopping Cart] from a [User]
+///
+/// It an Identifier [id], a [list] of [products] and a User Identifier [userId]
+///
+/// It also has methods to add an item to the cart, remove an item and deleting and item
+///
+/// Difference between [removing] and [deleting] an item: [removing] means [decrementing]
+/// one unit of an item. E.g: if a [user] with a [cart] containing [10 units] of a specific
+/// products [removes] one of them, then the [quantity] will reduce to 9,
+///
+/// But if the [user] [deletes] an [item], the [item] will be [removed completely] from
+/// the cart, [independent of the quantity]
 class Cart {
   final int id;
   final List<CartItem> products;
@@ -9,6 +21,7 @@ class Cart {
 
   Cart({required this.id, required this.products, required this.userId});
 
+  /// Returns an instance of a [Cart] based on the given [json]
   factory Cart.fromJson(Map<String, dynamic> json) {
     return Cart(
       id: json['id'],
@@ -20,18 +33,22 @@ class Cart {
     );
   }
 
+  /// Get the total quantity of unique products
   int get totalProducts {
     return products.length;
   }
 
+  /// Get the total quantity of products
   int get totalQuantity {
     return products.fold(0, (sum, product) => sum += product.quantity);
   }
 
+  /// Get the total price of all products
   double get total {
     return products.fold(0, (sum, product) => sum += product.total);
   }
 
+  /// Adds a [CartItem] to the [products] list
   void addItem({required CartItem cartItem}) {
     CartItem? existingItem = products.firstWhereOrNull(
       (item) => item == cartItem,
@@ -45,9 +62,10 @@ class Cart {
     existingItem.quantity++;
   }
 
+  /// Removes a [CartItem] from the [products] list
   void removeItem({required CartItem cartItem}) {
     CartItem? existingItem = products.firstWhereOrNull(
-          (item) => item == cartItem,
+      (item) => item == cartItem,
     );
 
     if (products.isEmpty || existingItem == null) {
@@ -62,13 +80,14 @@ class Cart {
     existingItem.quantity--;
   }
 
+  /// Deletes a [CartItem] from the [products] list
   void deleteItem({required CartItem cartItem}) {
     CartItem? existingItem = products.firstWhereOrNull(
-          (item) => item == cartItem,
+      (item) => item == cartItem,
     );
 
     if (products.isEmpty || existingItem == null) {
-      throw Exception('Tried to remove an unexisting product from the cart');
+      throw Exception('Tried to delete an unexisting product from the cart');
     }
 
     products.remove(existingItem);
